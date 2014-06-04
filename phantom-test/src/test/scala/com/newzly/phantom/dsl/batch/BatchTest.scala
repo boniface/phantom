39fb5b362607a82fea29fcdfadff7b58cc7acb58
Flavian Alexander <alexandruflavian23@gmail.com>
2014-06-04 22:02:43 +0100

    Adding more tests to BatchStatements

--- phantom-test/src/test/scala/com/newzly/phantom/dsl/batch/BatchTest.scala
+++ phantom-test/src/test/scala/com/newzly/phantom/dsl/batch/BatchTest.scala
@@ -25,7 +25,7 @@ import com.newzly.util.testing.cassandra.BaseTest
 
 
 class BatchTest extends BaseTest {
-  val keySpace: String = "BatchTestSpace"
+  val keySpace: String = "batch_tests"
   implicit val s: PatienceConfiguration.Timeout = timeout(10 seconds)
 
   override def beforeAll(): Unit = {
@@ -35,7 +35,7 @@ class BatchTest extends BaseTest {
     }
   }
 
-  it should "work fine" in {
+  it should "correctly execute a batch sequence" in {
     val row = JodaRow.sample
     val row2 = JodaRow.sample.copy(pkey = row.pkey)
     val row3 = JodaRow.sample
@@ -70,7 +70,46 @@ class BatchTest extends BaseTest {
         res._1.isDefined shouldEqual true
 
         res._1.get shouldEqual row
+        res._2.isEmpty shouldEqual true
+      }
+    }
+  }
 
+  it should "correctly execute a batch sequence with Twitter Futures" in {
+    val row = JodaRow.sample
+    val row2 = JodaRow.sample.copy(pkey = row.pkey)
+    val row3 = JodaRow.sample
+    PrimitivesJoda.insertSchema()
+    val statement1 = PrimitivesJoda.insert
+      .value(_.pkey, row.pkey)
+      .value(_.intColumn, row.int)
+      .value(_.timestamp, row.bi)
+    val statement2 = PrimitivesJoda.insert
+      .value(_.pkey, row3.pkey)
+      .value(_.intColumn, row3.int)
+      .value(_.timestamp, row3.bi)
+    val statement3 = PrimitivesJoda.update
+      .where(_.pkey eqs row2.pkey)
+      .modify(_.intColumn setTo row2.int)
+      .modify(_.timestamp setTo  row2.bi)
+    val statement4 = PrimitivesJoda.delete
+      .where(_.pkey eqs row3.pkey)
+
+    val batch = BatchStatement().add(statement3).add(statement4)
+
+    val w = for {
+      s1 <- statement1.execute()
+      s3 <- statement2.execute()
+      b <- batch.execute()
+      updated <- PrimitivesJoda.select.where(_.pkey eqs row.pkey).get()
+      deleted <- PrimitivesJoda.select.where(_.pkey eqs row3.pkey).get()
+    } yield (updated, deleted)
+
+    w successful {
+      res => {
+        res._1.isDefined shouldEqual true
+
+        res._1.get shouldEqual row
         res._2.isEmpty shouldEqual true
       }
     }
